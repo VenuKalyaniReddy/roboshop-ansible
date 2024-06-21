fa3aa1aa097348c11dadbed1fb625989a24114ca
unknown <kalyaninalamaru.com>
2024-06-21 15:47:11 +0530

    roboshop-ansible

--- awsRouteinstanceforroboshop.sh
+++ awsRouteinstanceforroboshop.sh
@@ -15,7 +15,7 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
- IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-0673d44a67505798d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+ IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-014eccb356d7ab4d2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
 
    #create R53 record, make sure you delete existing record
